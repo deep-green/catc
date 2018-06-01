3d2b329b1fdf5509b7b284d8e2a663fdf6ad14a0
martistuwe <martistuwe@yahoo.de>
2018-06-01 21:29:53 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let socket = io('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:5000');
+
+let wTurns = 1, bTurns = 0, active_player = 'w';
+
+let onDrop = function (source, target, piece, newPos, oldPos, orientation) {
+
+    active_player = (active_player === 'w') ? 'b':'w';
+    if(wTurns > bTurns) {
+        bTurns++;
+    } else {
+        wTurns++;
+    }
+
+    let fen = ChessBoard.objToFen(newPos) + ' ' + active_player + ' KQkq - ' + bTurns + ' ' + wTurns;
+    console.log(fen);
+    socket.emit('receive', {
+        FEN: fen,
+        ID_game: 2
+    })
+};
+
+socket.on('makeMove', data => {
+    board.move(data.substr(0,2) + '-' + data.substr(2,4));
+});
+
+let cfg = {
+    draggable: true,
+    position: 'start',
+    onDrop: onDrop
+};
+
+let board = ChessBoard('board', cfg);
